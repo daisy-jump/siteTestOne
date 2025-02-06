c3f76c1518fc5cdcf45bf117833ff5967de0252d
daisy-jump <88658844+daisy-jump@users.noreply.github.com>
2025-02-06 16:33:06 +0000

    work pls

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,12 +18,13 @@ const config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: 'https://daisy-jump.github.io/siteTestOne/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'daisy-jump', // Usually your GitHub org/user name.
+  projectName: 'siteTestOne', // Usually your repo name.
+  // deploymentBranch: 
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
