0e92cb2703c796f7606b56f59d7b6c4f4a45ff2b
daisy-jump <88658844+daisy-jump@users.noreply.github.com>
2025-02-06 16:49:36 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://daisy-jump.github.io/siteTestOne/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'https://daisy-jump.github.io/siteTestOne/',
+  baseUrl: '/siteTestOne/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
